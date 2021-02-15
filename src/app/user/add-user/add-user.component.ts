6689e83a28b103e794d5a391ed9df2c56e348aef
ParimalShete <parimalmshete@gmail.com>
2021-02-16 00:25:31 +0530

    first commit

---
+++ src/app/user/add-user/add-user.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ApiService} from "../../service/api.service";
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder,private router: Router, private apiService: ApiService) { }
+
+  addForm: FormGroup;
+
+  ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required],
+    });
+
+  }
+
+  onSubmit() {
+    this.apiService.createUser(this.addForm.value)
+      .subscribe( data => {
+        if(data.status === 200) {
+          alert('Student added successfully !!');
+          this.router.navigate(['list-user']);
+        }else {
+          alert("Unsuccessful");
+        }
+      },
+      error => {
+        alert("Error!!");
+      });
+  }
+
+}
